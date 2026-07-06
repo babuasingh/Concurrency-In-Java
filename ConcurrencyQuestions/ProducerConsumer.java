6ea6aebd10e03bb2f20a99842f0d644dbb9087b5
Siddharth <siddharthsingh02901@gmail.com>
2026-07-06 20:28:50 +0530

    Added different solutions to the ProducerConsumer problem .

--- ConcurrencyQuestions/ProducerConsumer.java
+++ ConcurrencyQuestions/ProducerConsumer.java
@@ -1,10 +1,132 @@
 package ConcurrencyQuestions;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+
+/*
+    Approach 1 : Threads -> Using wait and notify(1 P and 1 C)
+    Approach 2 : BlockingQueue -> Java inbuilt Queue for handling concurrency
+    Approach 3 : Executors -> when multiple P and C
+ */
 
 public class ProducerConsumer {
-    public List<Double> list = new ArrayList<>(); // buffer
+    public static void main(String[] args) throws InterruptedException {
+
+//        PCApproach1 obj1 = new PCApproach1();
+//        obj1.solve();
+//
+//        PCApproach2 obj2 = new PCApproach2();
+//        obj2.solve();
+//
+//        ProducerConsumerByMe obj = new ProducerConsumerByMe();
+//        obj.solve();
+    }
+}
+
+
+class PCApproach1 {
+
+    Queue<Integer> queue = new LinkedList<>();
+    int maxSize = 5;
+
+    public void solve() {
+        Thread producer = new Thread(()->{
+            for(int i=1;i<=10;i++) {
+                synchronized (queue) {
+                    while(queue.size()== maxSize) {
+                        System.out.println("Producer is waiting as queue is full");
+                        try {
+                            queue.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    int val =(int) (Math.random() * 100);
+                    queue.add(val);
+                    System.out.println("Produced Value : "+val);
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    queue.notifyAll();
+                }
+            }
+        },"producer");
+
+        Thread consumer = new Thread(()->{
+            for(int i=1;i<=10;i++) {
+                synchronized (queue) {
+                    while(queue.size()== 0) {
+                        System.out.println("Consumer is waiting to consume");
+                        try {
+                            queue.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    int val = queue.poll();
+                    System.out.println("Consumed : "+val);
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    queue.notifyAll();
+                }
+            }
+        },"consumer");
+
+        producer.start();
+        consumer.start();
+
+
+        try {
+            producer.join();
+            consumer.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Completed");
+    }
+}
+
+class PCApproach2 {
+    public void solve() {
+        BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(5);
+        Thread producer = new Thread(()->{
+            for(int i=0;i<10;i++) {
+                try {
+                    queue.put(i); // This will block if buffer is full
+                    System.out.println(System.nanoTime() + " Produced " + i);
+                    Thread.sleep((int)(Math.random()*1000));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread consumer = new Thread(()->{
+            for(int i=0;i<10;i++) {
+                try {
+                    Integer val = queue.take(); // This will block if buffer is empty
+                    System.out.println(System.nanoTime() + " Consumed " + val);
+                    Thread.sleep((int)(Math.random()*1000));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        producer.start();
+        consumer.start();
+    }
+}
+
+class ProducerConsumerByMe {
+    public Queue<Double> list = new LinkedList<>(); // buffer
     public int maxbuffersize = 5;
     public volatile boolean running = true;
 
@@ -20,7 +142,7 @@ public class ProducerConsumer {
                     }
                     if(!running) // make sures that it stops after seconds(mentioned in the main method)
                         break;
-                    Double num = Math.random();
+                    Double num = Math.random()*100;
                     list.add(num);
                     System.out.println(Thread.currentThread().getName() + " has added a task : " + num + " |  current Jobs : " + list.size());
                     list.notifyAll();
@@ -42,8 +164,8 @@ public class ProducerConsumer {
                         list.wait();
                     }
                     if(!running) // make sures that it stops after seconds(mentioned in the main method)
-                            break;
-                    Double val = list.remove(list.size() - 1);
+                        break;
+                    Double val = list.poll();
                     System.out.println(Thread.currentThread().getName() + " is consuming the value " + val + " |  current Jobs : " + list.size());
                     list.notifyAll();
                 }
@@ -61,9 +183,9 @@ public class ProducerConsumer {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public void solve() throws InterruptedException {
 
-        ProducerConsumer pc = new ProducerConsumer();
+        ProducerConsumerByMe pc = new ProducerConsumerByMe();
 
         int noOfConsumers = 3;
 
